60d266778375c13d3050d269289815849a223849
Tobetch <takutobe@gmail.com>
2023-07-05 17:05:27 +0900

    add users module(template)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb+srv://<username>:<password>@cluster0.kngtf.mongodb.net/session-auth?retryWrites=true&w=majority',
     ),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
